43304516742ce000117670f358972cf1a373dedb
Puru <pcbabar777@gmail.com>
2023-11-25 22:13:14 +0530

    format indentation

--- Tenth/Functions.tf
+++ Tenth/Functions.tf
@@ -7,8 +7,8 @@ locals {
 }
 
 variable "tags" {
-  type = list
-  default = ["ec2-first","ec2-second"]
+  type    = list(any)
+  default = ["ec2-first", "ec2-second"]
 }
 
 variable "region" {
@@ -16,27 +16,27 @@ variable "region" {
 }
 
 variable "ami" {
-  type = map
+  type = map(any)
   default = {
     ap-south-1 = "ami-02a2af70a66af6dfb"
-    us-east-1 = "ami-02a2af70a66af6dff"
-    us-west-1 = "ami-02a2af70a66af6dfd"
+    us-east-1  = "ami-02a2af70a66af6dff"
+    us-west-1  = "ami-02a2af70a66af6dfd"
   }
 }
 
 resource "aws_key_pair" "login" {
-  key_name = "login-key"
+  key_name   = "login-key"
   public_key = file("${path.module}/id_rsa.pub")
 }
 
 resource "aws_instance" "web" {
-    ami = lookup(var.ami,var.region)
-    instance_type = "t2.micro"
-    key_name = aws_key_pair.login.key_name
-    count = 2
-    tags = {
-      Name = element(var.tags,count.index)
-    }
+  ami           = lookup(var.ami, var.region)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.login.key_name
+  count         = 2
+  tags = {
+    Name = element(var.tags, count.index)
+  }
 }
 
 output "currentime" {
